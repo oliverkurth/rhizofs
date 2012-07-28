166c5844c84f2a3bb79970acc7f286ba7a56642f
nmandery <nico@nmandery.net>
2012-07-28 17:12:37 +0200

    removing deprecated mapping code

--- src/mapping.c
+++ src/mapping.c
@@ -17,29 +17,6 @@ typedef struct flag_pair {
     int local;
 } flag_pair;
 
-/*
-static mode_pair mode_map_filetype[] = {
-    { RHI_FILETYPE_DIR,     S_IFDIR },
-    { RHI_FILETYPE_CHR,     S_IFCHR },
-    { RHI_FILETYPE_BLK,     S_IFBLK },
-    { RHI_FILETYPE_REG,     S_IFREG },
-    { RHI_FILETYPE_IFO,     S_IFIFO },
-    { RHI_FILETYPE_LNK,     S_IFLNK },
-    { RHI_FILETYPE_SOCK,    S_IFSOCK }
-};
-
-static mode_pair mode_map_perm[] = {
-    { RHI_PERM_RUSR,        S_IRUSR },
-    { RHI_PERM_WUSR,        S_IWUSR },
-    { RHI_PERM_XUSR,        S_IXUSR },
-    { RHI_PERM_RGRP,        S_IRGRP },
-    { RHI_PERM_WGRP,        S_IWGRP },
-    { RHI_PERM_XGRP,        S_IXGRP },
-    { RHI_PERM_ROTH,        S_IROTH },
-    { RHI_PERM_WOTH,        S_IWOTH },
-    { RHI_PERM_XOTH,        S_IXOTH }
-};
-*/
 
 static flag_pair errno_map[] = {
     { RHIZOFS__ERRNO__ERRNO_NONE,      0 },
@@ -63,70 +40,13 @@ static flag_pair errno_map[] = {
     { RHIZOFS__ERRNO__ERRNO_UNSERIALIZABLE,     EIO }
 };
 
-#define mode_map_len(mm) (sizeof(mm)/sizeof(mode_pair))
 #define flag_map_len(em) (sizeof(em)/sizeof(flag_pair))
 
 // Prototypes
 Rhizofs__PermissionSet * PermissionSet_create();
 void PermissionSet_destroy(Rhizofs__PermissionSet * permset);
 
-/*
-unsigned int
-mapping_mode_to_protocol(mode_t mode, int include_filetype)
-{
-    unsigned int md = 0000;
-    unsigned int i = 0;
 
-    if (include_filetype) {
-        for (i=0; i<mode_map_len(mode_map_filetype); ++i) {
-            if (mode_map_filetype[i].local & mode) {
-                md |= mode_map_filetype[i].protocol;
-                break; // break to avoid overwrite with other matching flags
-            }
-        }
-
-        if (md == 0000) {
-            md |= RHI_FILETYPE_REG; // fallback to regular file 
-        }
-    }
-
-    for (i=0; i<mode_map_len(mode_map_perm); ++i) {
-        if (mode_map_perm[i].local & mode) {
-            md |= mode_map_perm[i].protocol;
-        }
-    }
-    return md;
-};
-*/
-
-/*
-mode_t
-mapping_mode_from_protocol(unsigned int md, int include_filetype)
-{
-    mode_t mode = 0;
-    unsigned int i = 0;
-
-    if (include_filetype) {
-        for (i=0; i<mode_map_len(mode_map_filetype); ++i) {
-            if (mode_map_filetype[i].protocol & md) {
-                mode |= mode_map_filetype[i].local;
-                break; // break to avoid overwrite with other matching flags
-            }
-        }
-
-        if (mode == 0) {
-            mode |= S_IFREG; // fallback to regular file
-        }
-    }
-
-    for (i=0; i<mode_map_len(mode_map_perm); ++i) {
-        if (mode_map_perm[i].protocol & md) {
-            mode |= mode_map_perm[i].local;
-        }
-    }
-    return mode;
-};
-*/
 
 inline Rhizofs__PermissionSet *
 PermissionSet_create()
