de2ad025851ba2ba0a074ab207755935e99788df
Nico Mandery <nico@nmandery.net>
2012-09-04 21:33:23 +0200

    adding patch version number

--- src/version.h
+++ src/version.h
@@ -1,9 +1,14 @@
 #ifndef __version_h__
 #define __version_h__
 
+#include "helpers.h"
+
 #define RHI_VERSION_MAJOR 0
 #define RHI_VERSION_MINOR 1
-#define RHI_VERSION_FULL "0.1"
+#define RHI_VERSION_PATCH 1
+#define RHI_VERSION_FULL    STRINGIFY(RHI_VERSION_MAJOR) "." \
+                            STRINGIFY(RHI_VERSION_MINOR) "." \
+                            STRINGIFY(RHI_VERSION_PATCH)
 
 #define RHI_NAME "Rhizofs"
 #define RHI_NAME_LOWER "rhizofs"
