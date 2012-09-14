ca7915d8e6f0683abbbb24d2f364cdc247faae53
nmandery <nico@nmandery.net>
2012-09-14 10:29:37 +0200

    setting version number to 0.2.0

--- src/version.h
+++ src/version.h
@@ -4,8 +4,8 @@
 #include "helpers.h"
 
 #define RHI_VERSION_MAJOR 0
-#define RHI_VERSION_MINOR 1
-#define RHI_VERSION_PATCH 1
+#define RHI_VERSION_MINOR 2
+#define RHI_VERSION_PATCH 0
 #define RHI_VERSION_FULL    STRINGIFY(RHI_VERSION_MAJOR) "." \
                             STRINGIFY(RHI_VERSION_MINOR) "." \
                             STRINGIFY(RHI_VERSION_PATCH)
