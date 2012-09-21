f2563b7a8eb3d0cfaedcbfc76a03f5abb972a2d0
nmandery <nico@nmandery.net>
2012-09-21 12:07:24 +0200

    bump version number to 0.2.1

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 
 #define RHI_VERSION_MAJOR 0
 #define RHI_VERSION_MINOR 2
-#define RHI_VERSION_PATCH 0
+#define RHI_VERSION_PATCH 1
 #define RHI_VERSION_FULL    STRINGIFY(RHI_VERSION_MAJOR) "." \
                             STRINGIFY(RHI_VERSION_MINOR) "." \
                             STRINGIFY(RHI_VERSION_PATCH)
